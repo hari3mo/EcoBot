46a939630c1668cc46df583b1b3c3d685140721a
harisdsc <166474219+hari3mo@users.noreply.github.com>
2025-10-30 01:25:43 -0700

    run

--- run.py
+++ run.py
@@ -3,7 +3,10 @@ import subprocess
 
 if __name__ == "__main__":
     args = sys.argv
-    if args[1] == 'app':
+    if len(args) > 1:
+        if args[1] == 'logs':
+            subprocess.run(['python3', 'logs/logs.py'])
+        if args[1] == 'app':
+            subprocess.run(['python3', 'app/app.py'])
+    else:
         subprocess.run(['python3', 'app/app.py'])
-    elif args[1] == 'logs':
-        subprocess.run(['python3', 'logs/logs.py'])
