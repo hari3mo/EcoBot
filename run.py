c9d030d84b9ad042bb6f79954dc101db1a661779
harisdsc <166474219+hari3mo@users.noreply.github.com>
2025-10-30 14:36:11 -0700

    run

--- run.py
+++ run.py
@@ -3,18 +3,15 @@ import subprocess
 
 if __name__ == "__main__":
     args = sys.argv
-
+    scripts  = {'logs': 'logs/logs.py',
+                'charts':'charts/charts.py',
+                'app': 'app/app.py'}
     if len(args) > 1:
         script = args[1]
-        scripts  = {'logs': 'logs/logs.py', 'charts':'charts/charts.py'}
         if script == 'all':
-            for key in scripts:
+            for key in list(scripts.keys())[:-1]:
                 subprocess.run(['python3', scripts[key]])
-        if script == 'logs':
-            subprocess.run(['python3', 'logs/logs.py'])
-        if script == 'charts':
-            subprocess.run(['python3', 'charts/charts.py'])
-        if script == 'app':
-            subprocess.run(['python3', 'app/app.py'])
+        if script in scripts:
+            subprocess.run(['python3', scripts[script]])
     else:
-        subprocess.run(['python3', 'app/app.py'])
+        subprocess.run(['python3', scripts['app']])
